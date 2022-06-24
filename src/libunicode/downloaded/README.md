c8409813b2426ac566f54a43cfec9e0dbd67b1c9
Lily Scott <git@suchipi.tv>
2022-06-23 19:45:31 -0600

    Stop downloading the unicode files with tup
    It's nice in theory but annoying in practice

---
+++ src/libunicode/downloaded/README.md
@@ -0,0 +1,1 @@
+Run `./unicode_download.sh` in this folder to update these. But you usually won't need to.
---
+++ src/libunicode/downloaded/unicode_download.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+URL="ftp://ftp.unicode.org/Public/14.0.0/ucd"
+
+wget "$URL/CaseFolding.txt" -O "./CaseFolding.txt"
+wget "$URL/DerivedNormalizationProps.txt" -O "./DerivedNormalizationProps.txt"
+wget "$URL/PropList.txt" -O "./PropList.txt"
+wget "$URL/SpecialCasing.txt" -O "./SpecialCasing.txt"
+wget "$URL/CompositionExclusions.txt" -O "./CompositionExclusions.txt"
+wget "$URL/ScriptExtensions.txt" -O "./ScriptExtensions.txt"
+wget "$URL/UnicodeData.txt" -O "./UnicodeData.txt"
+wget "$URL/DerivedCoreProperties.txt" -O "./DerivedCoreProperties.txt"
+wget "$URL/NormalizationTest.txt" -O "./NormalizationTest.txt"
+wget "$URL/Scripts.txt" -O "./Scripts.txt"
+wget "$URL/PropertyValueAliases.txt" -O "./PropertyValueAliases.txt"
+
+# The url path for this one is subtly different
+wget "$URL/emoji/emoji-data.txt" -O "./emoji-data.txt"
