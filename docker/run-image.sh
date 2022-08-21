31518db73e53d648cce293e3d28dbd0db6411199
Lily Scott <git@suchipi.tv>
2022-08-21 00:53:46 -0600

    Missed a few spots

--- docker/run-image.sh
+++ docker/run-image.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ -z "$1" ]]; then
-  echo Please use the first command-line argument to this script to specify which image to run: 'linux', 'windows' or 'darwin'.
+  echo Please use the first command-line argument to this script to specify which image to run: 'linux-from-linux', 'windows-from-linux', 'darwin-from-linux', or 'darwin-arm-from-linux'.
 fi
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
