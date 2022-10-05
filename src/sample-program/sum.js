63a8d70de0d8a2069915c073d586fc5e21918fbf
Lily Skye <git@suchipi.tv>
2022-10-05 03:09:15 -0600

    Add sample program to show how to generate standalone binaries

---
+++ src/sample-program/sum.js
@@ -0,0 +1,17 @@
+import * as std from "std";
+
+function sum(nums) {
+  return nums.reduce((prev, curr) => prev + curr, 0);
+}
+
+const result = sum(
+  scriptArgs.slice(1).map((arg) => {
+    const num = parseFloat(arg);
+    if (Number.isNaN(num)) {
+      throw new Error("Not a number: " + arg);
+    }
+    return num;
+  })
+);
+
+std.out.puts(result + "\n");
