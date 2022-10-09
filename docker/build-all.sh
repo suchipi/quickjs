b39485b5fc68e1d8a3731496eaabd067dd791f32
Lily Skye <git@suchipi.tv>
2022-10-09 17:21:49 -0600

    Fix archives not being included in docker artifacts anymore

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -10,5 +10,5 @@ rm -rf ./docker/artifacts/*
 
 for VARIANT in darwin-from-linux darwin-arm-from-linux linux-from-linux windows-from-linux; do
   mkdir -p ./docker/artifacts/$VARIANT
-  ./docker/run-image.sh $VARIANT bash -c 'make && mv ./src/**/*.{target,target.*} ./docker/artifacts/'"$VARIANT"
+  ./docker/run-image.sh $VARIANT bash -c 'shopt -s globstar && make && mv ./src/**/*.{target,target.*} ./docker/artifacts/'"$VARIANT"
 done
