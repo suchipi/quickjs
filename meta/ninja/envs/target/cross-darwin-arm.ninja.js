ade6bb730ec1e14fd856741209b92fbfc982aad4
Lily Skye <git@suchipi.tv>
2022-11-18 17:05:27 -0700

    Fix HOST/TARGET mismatch in cross-darwin target envs

--- meta/ninja/envs/target/cross-darwin-arm.ninja.js
+++ meta/ninja/envs/target/cross-darwin-arm.ninja.js
@@ -2,7 +2,7 @@
 declare("CC_TARGET", "arm64-apple-darwin20.4-clang");
 declare("AR_TARGET", "arm64-apple-darwin20.4-ar");
 declare("DEFINES_TARGET", "-D__APPLE__");
-declare("LDFLAGS_HOST", "-rdynamic");
+declare("LDFLAGS_TARGET", "-rdynamic");
 declare("LIBS_TARGET", "-ldl -lpthread");
 declare(
   "CFLAGS_TARGET",
--- meta/ninja/envs/target/cross-darwin-x86.ninja.js
+++ meta/ninja/envs/target/cross-darwin-x86.ninja.js
@@ -2,7 +2,7 @@
 declare("CC_TARGET", "x86_64-apple-darwin20.4-clang");
 declare("AR_TARGET", "x86_64-apple-darwin20.4-ar");
 declare("DEFINES_TARGET", "-D__APPLE__");
-declare("LDFLAGS_HOST", "-rdynamic");
+declare("LDFLAGS_TARGET", "-rdynamic");
 declare("LIBS_TARGET", "-ldl -lpthread");
 declare(
   "CFLAGS_TARGET",
