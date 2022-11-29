7be9fd68ca9cea0d5550751bd33cd9cfa6fb89ff
Lily Skye <me@suchipi.com>
2022-11-29 04:18:02 -0700

    Have to turn off optimization to keep bootstrap stub size consistent
    todo later: only turn this off for the bootstrap stub binary, maybe?

--- meta/ninja/envs/host/linux.ninja.js
+++ meta/ninja/envs/host/linux.ninja.js
@@ -3,6 +3,5 @@ declare("LTO_HOST", "y");
 declare("CC_HOST", "gcc");
 declare("AR_HOST", "gcc-ar");
 declare("DEFINES_HOST", "-D_GNU_SOURCE -D__linux__");
-declare("CFLAGS_HOST", "-O2");
 declare("LDFLAGS_HOST", "-rdynamic");
 declare("LIBS_HOST", "-ldl -lpthread");
--- meta/ninja/envs/target/linux.ninja.js
+++ meta/ninja/envs/target/linux.ninja.js
@@ -3,6 +3,5 @@ declare("LTO_TARGET", "y");
 declare("CC_TARGET", "gcc");
 declare("AR_TARGET", "gcc-ar");
 declare("DEFINES_TARGET", "-D_GNU_SOURCE -D__linux__");
-declare("CFLAGS_TARGET", "-O2");
 declare("LDFLAGS_TARGET", "-rdynamic");
 declare("LIBS_TARGET", "-ldl -lpthread");
