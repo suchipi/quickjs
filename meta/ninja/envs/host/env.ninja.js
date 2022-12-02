9cee26cdc12706ea87f3fc054a9e91d5d66f820c
Lily Skye <me@suchipi.com>
2022-12-02 14:17:29 -0700

    make it possible for env... envs... to have host/target-specific env vars

--- meta/ninja/envs/host/env.ninja.js
+++ meta/ninja/envs/host/env.ninja.js
@@ -1,17 +1,17 @@
 // Host: anything (configure stuff yourself via env vars)
 
-declare("LTO_HOST", env.LTO || "n");
-declare("CC_HOST", env.CC || "cc");
-declare("AR_HOST", env.AR || "ar");
-declare("DEFINES_HOST", env.DEFINES || "");
-declare("CFLAGS_HOST", env.CFLAGS || "");
-declare("LDFLAGS_HOST", env.LDFLAGS || "");
-declare("LIBS_HOST", env.LIBS || "-ldl -lpthread");
+declare("LTO_HOST", env.LTO_HOST || env.LTO || "n");
+declare("CC_HOST", env.CC_HOST || env.CC || "cc");
+declare("AR_HOST", env.AR_HOST || env.AR || "ar");
+declare("DEFINES_HOST", env.DEFINES_HOST || env.DEFINES || "");
+declare("CFLAGS_HOST", env.CFLAGS_HOST || env.CFLAGS || "");
+declare("LDFLAGS_HOST", env.LDFLAGS_HOST || env.LDFLAGS || "");
+declare("LIBS_HOST", env.LIBS_HOST || env.LIBS || "-ldl -lpthread");
 
-console.error("LTO_HOST", env.LTO || "n");
-console.error("CC_HOST", env.CC || "cc");
-console.error("AR_HOST", env.AR || "ar");
-console.error("DEFINES_HOST", env.DEFINES || "");
-console.error("CFLAGS_HOST", env.CFLAGS || "");
-console.error("LDFLAGS_HOST", env.LDFLAGS || "");
-console.error("LIBS_HOST", env.LIBS || "-ldl -lpthread");
+console.error("LTO_HOST", getVar("LTO_HOST"));
+console.error("CC_HOST", getVar("CC_HOST"));
+console.error("AR_HOST", getVar("AR_HOST"));
+console.error("DEFINES_HOST", getVar("DEFINES_HOST"));
+console.error("CFLAGS_HOST", getVar("CFLAGS_HOST"));
+console.error("LDFLAGS_HOST", getVar("LDFLAGS_HOST"));
+console.error("LIBS_HOST", getVar("LIBS_HOST"));
