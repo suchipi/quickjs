8d2cdcca6317d392b73d1ca32c7a76dfdf73825a
Lily Skye <git@suchipi.tv>
2022-12-02 09:56:05 -0700

    fix windows binaries mistakenly not being statically linked

--- meta/ninja/envs/target/cross-windows-x86_64.ninja.js
+++ meta/ninja/envs/target/cross-windows-x86_64.ninja.js
@@ -6,6 +6,6 @@ declare("AR_TARGET", "x86_64-w64-mingw32-gcc-ar");
 declare("DEFINES_TARGET", "-D_GNU_SOURCE -D__USE_MINGW_ANSI_STDIO -D_WIN32");
 declare("LIBS_TARGET", "-lpthread");
 // statically-linked so that it doesn't depend on libwinpthread-1.dll
-declare("CFLAGS_TARGET", "-static");
+declare("LDFLAGS_TARGET", "-static");
 
 declare("DOTEXE", ".exe");
