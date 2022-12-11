db22dd0eb625aea8fb6cf6feac7405b87fd83ca1
Lily Skye <me@suchipi.com>
2022-12-11 02:00:27 -0700

    improve qjsbootstrap argv0 test so it tests the after-appended case, too

--- tests/qjsbootstrap-argv0.test.ts
+++ tests/qjsbootstrap-argv0.test.ts
@@ -1,10 +1,18 @@
+import path from "path";
+import { cp, rm, mkdir, echo } from "shelljs";
 import { spawn } from "first-base";
-import { binDir } from "./_utils";
+import { binDir, rootDir } from "./_utils";
+
+const workdir = rootDir("build/tests/qjsbootstrap-argv0");
+
+beforeEach(() => {
+  rm("-rf", workdir);
+  mkdir("-p", workdir);
+});
 
 test("qjsbootstrap - finds itself properly when argv0 is indirect", async () => {
   const run = spawn(binDir("qjsbootstrap"), {
     argv0: "something not particularly helpful",
-    cwd: "/tmp",
   });
   await run.completion;
   expect(run.result).toMatchInlineSnapshot(`
@@ -13,6 +21,24 @@ test("qjsbootstrap - finds itself properly when argv0 is indirect", async () => 
       "error": false,
       "stderr": "",
       "stdout": "append UTF-8 encoded JavaScript to the end of this binary to change this binary into a program that executes that JavaScript code
+    ",
+    }
+  `);
+
+  const prog = path.join(workdir, "myprog");
+  cp(binDir("qjsbootstrap"), prog);
+  echo("console.log(2 + 2)").toEnd(prog);
+
+  const run2 = spawn(prog, {
+    argv0: "something not particularly helpful",
+  });
+  await run2.completion;
+  expect(run2.result).toMatchInlineSnapshot(`
+    {
+      "code": 0,
+      "error": false,
+      "stderr": "",
+      "stdout": "4
     ",
     }
   `);
