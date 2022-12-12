9d9e1635406afc781d2a9d08f0c3c4c425ed0a74
Lily Skye <git@suchipi.tv>
2022-12-11 19:25:54 -0700

    Add String.dedent to the .d.ts

--- src/quickjs-libc/quickjs-libc.d.ts
+++ src/quickjs-libc/quickjs-libc.d.ts
@@ -1027,9 +1027,53 @@ declare function clearInterval(interval: Interval): void;
 interface StringConstructor {
   /**
    * A no-op template literal tag.
+   *
+   * https://github.com/tc39/proposal-string-cooked
    */
   cooked(
     strings: readonly string[] | ArrayLike<string>,
     ...substitutions: any[]
   ): string;
+
+  /**
+   * Remove leading minimum indentation from the string.
+   * The first line of the string must be empty.
+   *
+   * https://github.com/tc39/proposal-string-dedent
+   */
+  dedent: {
+    /**
+     * Remove leading minimum indentation from the string.
+     * The first line of the string must be empty.
+     *
+     * https://github.com/tc39/proposal-string-dedent
+     */
+    (input: string): string;
+
+    /**
+     * Remove leading minimum indentation from the template literal.
+     * The first line of the string must be empty.
+     *
+     * https://github.com/tc39/proposal-string-dedent
+     */
+    (
+      strings: readonly string[] | ArrayLike<string>,
+      ...substitutions: any[]
+    ): string;
+
+    /**
+     * Wrap another template tag function such that tagged literals
+     * become dedented before being passed to the wrapped function.
+     *
+     * https://www.npmjs.com/package/string-dedent#usage
+     */
+    <
+      Func extends (
+        strings: readonly string[] | ArrayLike<string>,
+        ...substitutions: any[]
+      ) => string
+    >(
+      input: Func
+    ): Func;
+  };
 }
