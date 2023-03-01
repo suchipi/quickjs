8c719c6d62e74351192555c3d6587a2d833f39b9
Lily Skye <me@suchipi.com>
2023-03-01 02:40:29 -0700

    Add (very basic) typescript types for `Operators` global

--- src/quickjs/quickjs.d.ts
+++ src/quickjs/quickjs.d.ts
@@ -35,4 +35,105 @@ interface SymbolConstructor {
    *   determining the result of the `typeof` operation (it'll ignore it).
    */
   readonly typeofValue: unique symbol;
+
+  /**
+   * To override operators (+, -, ==, etc) for an object, set its
+   * `Symbol.operatorSet` property to an `OperatorSet` object, which can be
+   * created via `Operators.create`.
+   */
+  readonly operatorSet: unique symbol;
+}
+
+/**
+ * An object that, if placed on another object's `Symbol.operatorSet` property,
+ * will overload its operators to behave as defined by the functions this
+ * OperatorSet was constructed with.
+ *
+ * You can create an OperatorSet via `Operators(...)` or
+ * `Operators.create(...)`.
+ */
+declare type OperatorSet = {
+  /** This property is not here at runtime; we just use it to make this type differ from an empty object */
+  __is__: "OperatorSet";
+};
+
+interface OperatorFunctions<Left, Right> {
+  "+": (left: Left, right: Right) => any;
+  "-": (left: Left, right: Right) => any;
+  "*": (left: Left, right: Right) => any;
+  "/": (left: Left, right: Right) => any;
+  "%": (left: Left, right: Right) => any;
+  "**": (left: Left, right: Right) => any;
+  "|": (left: Left, right: Right) => any;
+  "&": (left: Left, right: Right) => any;
+  "^": (left: Left, right: Right) => any;
+  "<<": (left: Left, right: Right) => any;
+  ">>": (left: Left, right: Right) => any;
+  ">>>": (left: Left, right: Right) => any;
+  "==": (left: Left, right: Right) => any;
+  "<": (left: Left, right: Right) => any;
+  pos: (left: Left, right: Right) => any;
+  neg: (left: Left, right: Right) => any;
+  "++": (left: Left, right: Right) => any;
+  "--": (left: Left, right: Right) => any;
+  "~": (left: Left, right: Right) => any;
+}
+
+interface SelfOperators<T> extends Partial<OperatorFunctions<T, T>> {
+  left?: undefined;
+  right?: undefined;
+}
+
+interface LeftOperators<T, Left> extends Partial<OperatorFunctions<Left, T>> {
+  left: {};
+  right?: undefined;
+}
+
+interface RightOperators<T, Right>
+  extends Partial<OperatorFunctions<T, Right>> {
+  left?: undefined;
+  right: {};
+}
+
+interface OperatorsConstructor {
+  /**
+   * Creates a new OperatorSet object, which should be placed on an object's
+   * Symbol.operatorSet property.
+   */
+  <T>(
+    selfOperators?: SelfOperators<T>,
+    ...otherOperators: Array<LeftOperators<T, any> | RightOperators<T, any>>
+  ): OperatorSet;
+
+  /**
+   * Creates a new OperatorSet object, which should be placed on an object's
+   * Symbol.operatorSet property.
+   */
+  create: <T>(
+    selfOperators?: SelfOperators<T>,
+    ...otherOperators: Array<LeftOperators<T, any> | RightOperators<T, any>>
+  ) => OperatorSet;
+
+  /** In math mode, the BigInt division and power operators can be overloaded by using this function. */
+  updateBigIntOperators(
+    ops: Pick<OperatorFunctions<BigInt, BigInt>, "/" | "**">
+  ): void;
+}
+
+declare var Operators: OperatorsConstructor;
+
+interface Number {
+  [Symbol.operatorSet]: OperatorSet;
+}
+
+interface Boolean {
+  [Symbol.operatorSet]: OperatorSet;
+}
+
+interface String {
+  [Symbol.operatorSet]: OperatorSet;
+}
+
+interface BigInt {
+  [Symbol.operatorSet]: OperatorSet;
 }
