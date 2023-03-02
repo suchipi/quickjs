12ce10c8697630ad8c2fbf2f60422b21af8bf318
Lily Skye <me@suchipi.com>
2023-03-02 12:09:06 -0700

    add typedefs for BigInt extensions

--- src/quickjs/quickjs.d.ts
+++ src/quickjs/quickjs.d.ts
@@ -137,3 +137,94 @@ interface String {
 interface BigInt {
   [Symbol.operatorSet]: OperatorSet;
 }
+
+interface BigIntConstructor {
+  /**
+   * Return trunc(a/b).
+   *
+   * b = 0 raises a RangeError exception.
+   */
+  tdiv(a: bigint, b: bigint): bigint;
+
+  /**
+   * Return \lfloor a/b \rfloor.
+   *
+   * b = 0 raises a RangeError exception.
+   */
+  fdiv(a: bigint, b: bigint): bigint;
+
+  /**
+   * Return \lceil a/b \rceil.
+   *
+   * b = 0 raises a RangeError exception.
+   */
+  cdiv(a: bigint, b: bigint): bigint;
+
+  /**
+   * Return sgn(b) \lfloor a/{|b|} \rfloor (Euclidian division).
+   *
+   * b = 0 raises a RangeError exception.
+   */
+  ediv(a: bigint, b: bigint): bigint;
+
+  /**
+   * Perform trunc(a/b) and return an array of two elements. The first element
+   * is the quotient, the second is the remainder.
+   *
+   * b = 0 raises a RangeError exception.
+   */
+  tdivrem(a: bigint, b: bigint): [bigint, bigint];
+
+  /**
+   * Perform \lfloor a/b \rfloor and return an array of two elements. The first
+   * element is the quotient, the second is the remainder.
+   *
+   * b = 0 raises a RangeError exception.
+   */
+  fdivrem(a: bigint, b: bigint): [bigint, bigint];
+
+  /**
+   * Perform \lceil a/b \rceil and return an array of two elements. The first
+   * element is the quotient, the second is the remainder.
+   *
+   * b = 0 raises a RangeError exception.
+   */
+  cdivrem(a: bigint, b: bigint): [bigint, bigint];
+
+  /**
+   * Perform sgn(b) \lfloor a/{|b|} \rfloor (Euclidian division) and return an
+   * array of two elements. The first element is the quotient, the second is
+   * the remainder.
+   *
+   * b = 0 raises a RangeError exception.
+   */
+  edivrem(a: bigint, b: bigint): [bigint, bigint];
+
+  /**
+   * Return \lfloor \sqrt(a) \rfloor.
+   *
+   * A RangeError exception is raised if a < 0.
+   */
+  sqrt(a: bigint): bigint;
+
+  /**
+   * Return an array of two elements. The first element is
+   * \lfloor \sqrt{a} \rfloor. The second element is
+   * a-\lfloor \sqrt{a} \rfloor^2.
+   *
+   * A RangeError exception is raised if a < 0.
+   */
+  sqrtrem(a: bigint): [bigint, bigint];
+
+  /**
+   * Return -1 if a \leq 0 otherwise return \lfloor \log2(a) \rfloor.
+   */
+  floorLog2(a: bigint): bigint;
+
+  /**
+   * Return the number of trailing zeros in the two’s complement binary representation of a.
+   *
+   * Return -1 if a=0.
+   */
+  ctz(a: bigint): bigint;
+}
