6d720cc3ccc808b3b6972b499fa0dd96c15df476
Lily Skye <me@suchipi.com>
2023-07-17 19:09:02 -0600

    prepare for publishing on npm

---
+++ npm/index.cjs
@@ -0,0 +1,67 @@
+const path = require("path");
+const child_process = require("child_process");
+
+exports.buildArtifactsLocation = function buildArtifactsLocation() {
+  return path.resolve(__dirname, "..", "build");
+};
+
+exports.identifyCurrentPlatform = function identifyCurrentPlatform() {
+  const platform = process.platform;
+  switch (platform) {
+    case "darwin":
+    case "linux":
+    case "win32": {
+      break;
+    }
+    default: {
+      throw new Error(`Unsupported platform: ${platform}`);
+    }
+  }
+
+  let arch = process.arch;
+  switch (arch) {
+    case "arm64": {
+      arch = "aarch64";
+      break;
+    }
+    case "x64": {
+      arch = "x86_64";
+      break;
+    }
+    default: {
+      throw new Error(`Unsupported processor architecture: ${arch}`);
+    }
+  }
+
+  switch (platform) {
+    case "darwin": {
+      return `${arch}-apple-darwin`;
+    }
+    case "win32": {
+      if (arch !== "x86_64") {
+        throw new Error(`Architecture is not supported on Windows: ${arch}`);
+      }
+      return `${arch}-pc-windows-static`;
+    }
+    case "linux": {
+      const lddInfo = runShell(`ldd --version 2>&1 || true`);
+      if (/musl/.test(lddInfo)) {
+        return `${arch}-unknown-linux-musl`;
+      } else if (/GLIBC|GNU/.test(lddInfo)) {
+        return `${arch}-unknown-linux-gnu`;
+      } else {
+        return `${arch}-unknown-linux-static`;
+      }
+    }
+    default: {
+      // should be unreachable
+      throw new Error(`Unsupported platform: ${platform}`);
+    }
+  }
+};
+
+function runShell(cmd) {
+  return child_process
+    .spawnSync(cmd, { shell: true, encoding: "utf-8" })
+    .stdout.trim();
+}
