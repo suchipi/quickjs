0a9a3db46719146d4abc373c54815ae12db1d427
Lily Skye <git@suchipi.tv>
2023-08-26 15:26:11 -0600

    fix windows build failing due to localtime_r missing

--- meta/ninja/envs/target/cross-windows-x86_64.ninja.js
+++ meta/ninja/envs/target/cross-windows-x86_64.ninja.js
@@ -2,8 +2,15 @@
 declare("LTO_TARGET", "y");
 declare("CC_TARGET", "x86_64-w64-mingw32-gcc");
 declare("AR_TARGET", "x86_64-w64-mingw32-gcc-ar");
-// __USE_MINGW_ANSI_STDIO for standard snprintf behavior
-declare("DEFINES_TARGET", "-D_GNU_SOURCE -D__USE_MINGW_ANSI_STDIO -D_WIN32");
+declare("DEFINES_TARGET", [
+  "-D_WIN32",
+  // to get environ
+  "-D_GNU_SOURCE",
+  // to get standard snprintf behavior
+  "-D__USE_MINGW_ANSI_STDIO",
+  // to get localtime_r
+  "-D_POSIX_THREAD_SAFE_FUNCTIONS",
+]);
 // statically-linked so that it doesn't depend on libwinpthread-1.dll
 declare("LDEXPORT_TARGET", "-static");
 
