6acaef69950d48fa03d1d178852da8da693f363b
Lily Scott <me@suchipi.com>
2023-09-10 23:06:46 -0600

    add arm64 support to cosmo output

--- meta/ninja/envs/target/cosmo.ninja.js
+++ meta/ninja/envs/target/cosmo.ninja.js
@@ -2,7 +2,7 @@
 
 declare("CC_TARGET", "cosmocc");
 declare("AR_TARGET", "x86_64-unknown-cosmo-ar");
-declare("DEFINES_TARGET", "-D_GNU_SOURCE");
+declare("DEFINES_TARGET", "-D_GNU_SOURCE -D__COSMO__");
 declare("LDEXPORT_TARGET", "-rdynamic");
 
 declare("PROGRAM_SUFFIX", ".com");
