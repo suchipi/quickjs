9e6d855f5b6d18186a1c784e6962be9a635240b1
Lily Skye <me@suchipi.com>
2023-09-17 01:29:07 -0600

    enable PIC in freeBSD targets
    needed in order to build extras

--- meta/ninja/envs/host/freebsd.ninja.js
+++ meta/ninja/envs/host/freebsd.ninja.js
@@ -3,3 +3,4 @@
 declare("CC_HOST", "cc");
 declare("AR_HOST", "ar");
 declare("LDEXPORT_HOST", "-rdynamic");
+declare("CFLAGS_HOST", "-fPIC");
--- meta/ninja/envs/target/freebsd.ninja.js
+++ meta/ninja/envs/target/freebsd.ninja.js
@@ -3,3 +3,4 @@
 declare("CC_TARGET", "cc");
 declare("AR_TARGET", "ar");
 declare("LDEXPORT_TARGET", "-rdynamic");
+declare("CFLAGS_TARGET", "-fPIC");
