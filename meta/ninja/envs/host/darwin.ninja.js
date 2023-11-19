a4a6c696e3ce76b7456d7bbfef90b6222d959218
Lily Skye <me@suchipi.com>
2023-11-18 23:15:07 -0700

    fix macOS tests
    a lot of these CLI flags aren't needed anymore... confusingly

--- meta/ninja/envs/host/darwin.ninja.js
+++ meta/ninja/envs/host/darwin.ninja.js
@@ -4,8 +4,5 @@ declare("CC_HOST", "clang");
 declare("AR_HOST", "ar");
 declare("DEFINES_HOST", "-D__APPLE__");
 declare("LDEXPORT_HOST", "-rdynamic");
-declare(
-  "CFLAGS_HOST",
-  "-Ofast -funsigned-char -Wno-unused-command-line-argument -fPIC -Wno-implicit-int-float-conversion"
-);
+declare("CFLAGS_HOST", "-O2");
 declare("SHARED_LIBRARY_FLAGS_HOST", "-undefined dynamic_lookup");
--- meta/ninja/envs/target/darwin.ninja.js
+++ meta/ninja/envs/target/darwin.ninja.js
@@ -4,8 +4,5 @@ declare("CC_TARGET", "clang");
 declare("AR_TARGET", "ar");
 declare("DEFINES_TARGET", "-D__APPLE__");
 declare("LDEXPORT_TARGET", "-rdynamic");
-declare(
-  "CFLAGS_TARGET",
-  "-Ofast -funsigned-char -Wno-unused-command-line-argument -fPIC -Wno-implicit-int-float-conversion"
-);
+declare("CFLAGS_TARGET", "-O2");
 declare("SHARED_LIBRARY_FLAGS_TARGET", "-undefined dynamic_lookup");
