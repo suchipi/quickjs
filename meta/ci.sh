1fa02186a2bfeab8dc9cd3327a56f5178efa64ed
Lily Skye <me@suchipi.com>
2024-01-31 04:43:31 -0700

    try a workaround

--- meta/ci.sh
+++ meta/ci.sh
@@ -10,7 +10,11 @@ elif [[ "$(uname)" == "Linux" ]]; then
 fi
 
 export NVM_DIR="$HOME/.nvm"
+# have to unset "e" when sourcing nvm in macOS due to weird issue described
+# here: https://github.com/nvm-sh/nvm/issues/1985#issuecomment-456022013
+set +e
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+set -e
 
 nvm install
 nvm use
