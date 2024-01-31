43bafdb7a9f440ed4d1dc720044b58f4c0b487f4
Lily Skye <me@suchipi.com>
2024-01-31 04:38:41 -0700

    nah that true didn't matter. let's try sourcing nvm the way it expects

--- meta/ci.sh
+++ meta/ci.sh
@@ -4,13 +4,13 @@ set -e
 if [[ "$(uname)" == "Darwin" ]]; then
   brew install ninja
   # install nvm
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash || true
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 elif [[ "$(uname)" == "Linux" ]]; then
   sudo apt-get install -y ninja-build
 fi
 
 export NVM_DIR="$HOME/.nvm"
-source "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 nvm install
 nvm use
