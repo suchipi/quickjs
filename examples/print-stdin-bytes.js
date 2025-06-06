d5fc8f52319b856c68d6b7966444f942e694563d
Lily Skye <git@suchipi.tv>
2025-06-06 00:37:33 -0600

    fix reference path

--- examples/print-stdin-bytes.js
+++ examples/print-stdin-bytes.js
@@ -1,5 +1,5 @@
 ///<reference path="../src/quickjs/quickjs.d.ts" />
-///<reference path="../src/quickjs-libc/quickjs-libc.d.ts" />
+///<reference path="../src/builtin-modules/quickjs-libc/quickjs-libc.d.ts" />
 
 import * as std from "quickjs:std";
 import * as os from "quickjs:os";
