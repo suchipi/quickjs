9369acfe60f7e10fbba42cc9093a0d2e69cbb70d
Lily Skye <me@suchipi.com>
2025-09-28 17:40:23 -0600

    get msys2 stuff working! :D

--- meta/ninja/envs/host/msys2.ninja.js
+++ meta/ninja/envs/host/msys2.ninja.js
@@ -2,5 +2,10 @@
 
 declare("CC_HOST", "gcc");
 declare("AR_HOST", "ar");
-declare("LDEXPORT_HOST", "-rdynamic");
+declare("LDEXPORT_HOST", "-static");
 declare("PROGRAM_SUFFIX_HOST", ".exe");
+
+// needed for localtime_r
+declare("DEFINES_HOST", "-D_POSIX_C_SOURCE");
+// needed for __imp_gethostname
+declare("LIBS_HOST", "-lws2_32");
--- meta/ninja/envs/target/msys2.ninja.js
+++ meta/ninja/envs/target/msys2.ninja.js
@@ -2,5 +2,10 @@
 
 declare("CC_TARGET", "gcc");
 declare("AR_TARGET", "ar");
-declare("LDEXPORT_TARGET", "-rdynamic");
+declare("LDEXPORT_TARGET", "-static");
 declare("PROGRAM_SUFFIX_TARGET", ".exe");
+
+// needed for localtime_r
+declare("DEFINES_TARGET", "-D_POSIX_C_SOURCE");
+// needed for __imp_gethostname
+declare("LIBS_TARGET", "-lws2_32");
