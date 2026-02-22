00a931afeaa3206a8f3a2805203a114fadc0610e
Lily Skye <git@suchipi.tv>
2026-02-21 20:45:21 -0700

    fix snapshot

--- tests/worker-exit.test.ts
+++ tests/worker-exit.test.ts
@@ -6,14 +6,12 @@ const testFixturesDir = fixturesDir.concat("worker-exit");
 test("cmdline.exit in worker throws", async () => {
   const run = spawn(binDir("qjs"), [testFixturesDir("main.js")]);
   await run.completion;
-  expect(run.result).toMatchInlineSnapshot(`
+  expect(run.cleanResult()).toMatchInlineSnapshot(`
     {
       "code": 0,
       "error": false,
       "stderr": "Error: cmdline.exit can only be called from the main thread
-        at <internal>/quickjs-cmdline.c:42
-        at exit (native)
-        at <anonymous> (/Users/suchipi/Code/quickjs/tests/fixtures/worker-exit/worker.js:9)
+        at somewhere
 
     ",
       "stdout": "in main
