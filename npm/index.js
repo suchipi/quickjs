3e460aea1030de88789f93a5bd3a0f343e7e2085
Lily Skye <git@suchipi.tv>
2026-03-13 05:00:54 -0600

    add freebsd binaries to platforms.json and make identifyCurrentPlatform prefer static abi

--- npm/index.js
+++ npm/index.js
@@ -10,12 +10,16 @@ const platforms = require("./platforms.json");
 exports.platforms = platforms;
 
 function identifyCurrentPlatform() {
-  const matchingPlatform = platforms.find((platform) => {
+  const matchingPlatforms = platforms.filter((platform) => {
     return (
       platform.os === process.platform &&
       platform.architectures.includes(process.arch)
     );
   });
+
+  const matchingPlatform =
+    matchingPlatforms.find((platform) => platform.abi === "static") ||
+    matchingPlatforms[0];
 
   if (matchingPlatform) {
     return matchingPlatform;
