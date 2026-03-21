e98d409b718d18672ff575461707c13a042c02a9
Lily Skye <git@suchipi.tv>
2026-03-20 20:23:33 -0600

    Promises are broken :(

---
+++ tests/promises.test.ts
@@ -0,0 +1,55 @@
+import { spawn } from "first-base";
+import { binDir } from "./_utils";
+
+test("Promises work", async () => {
+  const run = spawn(
+    binDir("qjs"),
+    [
+      "-e",
+      `
+        console.log("one");
+        Promise.resolve(true).then(() => {
+          console.log("two");
+        });
+
+        {
+          var resolve, reject;
+          const p = new Promise((res, rej) => {
+            resolve = res;
+            reject = rej;
+          });
+          p.then(() => {
+            console.log("three");
+          });
+          resolve(5);
+        }
+
+        {
+          var resolve, reject;
+          const p = new Promise((res, rej) => {
+            resolve = res;
+            reject = rej;
+          });
+          p.catch((err) => {
+            console.log("four", err);
+          });
+          reject(new Error("nah"));
+        }
+      `,
+    ],
+    { cwd: __dirname }
+  );
+  await run.completion;
+  expect(run.result).toMatchInlineSnapshot(`
+    {
+      "code": 0,
+      "error": false,
+      "stderr": "",
+      "stdout": "one
+      two
+      three
+      four Error: "nah"
+    ",
+    }
+  `);
+});
