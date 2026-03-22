807ad77c74d846738b4983671fea7f494a05d8ac
Lily Skye <git@suchipi.tv>
2026-03-22 02:33:56 -0600

    fix incorrect filename in stack frame

--- src/globals/quickjs-inspect/quickjs-inspect.c
+++ src/globals/quickjs-inspect/quickjs-inspect.c
@@ -9,7 +9,7 @@ int js_inspect_add_inspect_global(JSContext *ctx)
   int ret;
   JSSyntheticStackFrame *ssf;
 
-  ssf = JS_PushSyntheticStackFrame(ctx, "js_inspect_add_inspect_global", "quickjs-modulesys.c", __LINE__);
+  ssf = JS_PushSyntheticStackFrame(ctx, "js_inspect_add_inspect_global", "quickjs-inspect.c", __LINE__);
 
   ret = QJMS_EvalBinary(ctx, qjsc_inspect, qjsc_inspect_size, 0);
   JS_PopSyntheticStackFrame(ctx, ssf);
