7dda9a1385e4214aa43bc2107c576bf0be5949a3
Lily Skye <git@suchipi.tv>
2026-04-22 01:54:10 -0600

    port upstream d9c699f: fix class method with name get (#258)
    upstream: d9c699f528755360e32f7d2755ad3f91b575b4f9
    
    Clean apply. Added test cases for class field named `get` and
    object literal with `get`/`set`/`async` as data property names
    alongside a `get` accessor.

--- tests/oldtests/test_language.js
+++ tests/oldtests/test_language.js
@@ -339,6 +339,11 @@ function test_class() {
   assert(S.x === 42);
   assert(S.y === 42);
   assert(S.z === 42);
+
+  class P {
+    get = () => "123";
+  }
+  assert(new P().get() === "123");
 }
 
 function test_template() {
@@ -366,8 +371,9 @@ function test_object_literal() {
     get = 1,
     set = 2;
   async = 3;
-  a = { get: 2, set: 3, async: 4 };
-  assert(JSON.stringify(a), '{"get":2,"set":3,"async":4}');
+  a = { get: 2, set: 3, async: 4, get a(){ return this.get} };
+  assert(JSON.stringify(a), '{"get":2,"set":3,"async":4,"a":2}');
+  assert(a.a === 2);
 
   a = { x, get, set, async };
   assert(JSON.stringify(a), '{"x":0,"get":1,"set":2,"async":3}');
