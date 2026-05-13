dcb6775d95142919872ef1bc1795593ddd726748
Lily Skye <git@suchipi.tv>
2026-05-12 22:11:29 -0600

    port upstream 64c55c6: removed JS_PROP_NO_ADD
    upstream: 64c55c6

--- src/quickjs/quickjs.h
+++ src/quickjs/quickjs.h
@@ -387,8 +387,7 @@ JSClassID JS_VALUE_GET_CLASS_ID(JSValue v);
    (JS_SetProperty) */
 #define JS_PROP_THROW_STRICT     (1 << 15)
 
-#define JS_PROP_NO_ADD           (1 << 16) /* internal use */
-#define JS_PROP_NO_EXOTIC        (1 << 17) /* internal use */
+#define JS_PROP_NO_EXOTIC        (1 << 16) /* internal use */
 
 #ifndef JS_DEFAULT_STACK_SIZE
 #define JS_DEFAULT_STACK_SIZE (1024 * 1024)
